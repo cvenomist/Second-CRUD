e012f375c873fdfc47fd25097eebf23500203b34
Sujay Singh <38438205+cvenomist@users.noreply.github.com>
2018-12-08 10:52:01 +0530

    Update readView.php

--- readView.php
+++ readView.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>No C Only RUD</title>
+    <title>CRUD</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body class="container p-3 mb-2 bg-light text-dark">
